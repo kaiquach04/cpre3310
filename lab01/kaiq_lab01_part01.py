5bcf71a8ffc0697a501d2158cc18e0e36baa6697
Kai Quach <kaiquach04@gmail.com>
2026-01-29 00:53:28 -0600

    part01 code

---
+++ lab01/kaiq_lab01_part01.py
@@ -0,0 +1,19 @@
+def modify(sentence: list[str], shift: int) -> str:
+    result = []
+    for char in sentence:
+        upperCase = char.upper()
+        ascii = ord(upperCase)
+        indexVal = ascii - 65
+        newPos = (indexVal + shift) % 26 # The wording threw me off but im assuming technically the shift value should be pos mathematically but result in negative (go left)
+        shiftedChar = chr(newPos + 65)
+        result.append(shiftedChar)
+    return ''.join(result)
+
+def main() -> None:
+    sentence: list[str] = list(input("Write a sentence without spaces: "))
+    shift: int = int(input("Enter the shift value (positive for mathematically shifting to the right, negative shift to left: "))
+    result: str = modify(sentence, shift)
+    print(f"Encrypted sentence: {result}")
+
+if __name__ == "__main__":
+    main()
